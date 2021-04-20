c8398b6f240b7900790ef551be9c7cb5ef66ad8e
Cleber Del Rei <cleberdelrei@gmail.com>
2021-04-19 21:38:53 -0300

    moving folders

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Lab Web"', () => {
-      expect(appController.getHello()).toBe('Lab Web');
+      expect(appController.getHello()).toBe('Lab Web!!!');
     });
   });
 });
