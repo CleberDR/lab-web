d87d616af54f9bf428d03ec5796a75ccc2acaca7
Cleber Del Rei <cleberdelrei@gmail.com>
2021-05-03 21:43:12 -0300

    Removing old entities

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 declare const module: any;
+
+config();
+const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+
+  console.log(`API Online. Access http://localhost:${PORT}`)
 
   if (module.hot) {
     module.hot.accept();
--- api/src/terms-acceptance/dto/update-terms-acceptance.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTermsAcceptanceDto } from './create-terms-acceptance.dto';
-
-export class UpdateTermsAcceptanceDto extends PartialType(CreateTermsAcceptanceDto) {}
