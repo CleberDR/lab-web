4a195115d4b238cc68d0b23b47f938f58238b56d
Cleber Del Rei <cleberdelrei@gmail.com>
2021-05-10 17:06:47 -0300

    fix main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
 declare const module: any;
 
 config();
@@ -10,9 +11,25 @@ const PORT = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe);
-  await app.listen(PORT);
 
-  console.log(`API Online. Access http://localhost:${PORT}`)
+  const config = new DocumentBuilder()
+  .setBasePath('')
+  .setTitle('LabWeb API')
+  .setDescription('LabWeb Class API')
+  .setVersion('1.0')
+  .addTag('')
+  .build();
+  const options: SwaggerDocumentOptions =  {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
+  console.log(`API Online. Access http://localhost:${PORT}`);
 
   if (module.hot) {
     module.hot.accept();
